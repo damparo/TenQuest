d65ea4edae1fcf65e5f27371ce1b91c0f6592bf0
Dennis Amparo <dennisamparo11@gmail.com>
2020-11-11 14:38:52 -0700

    server routes

--- server.js
+++ server.js
@@ -1,12 +1,28 @@
-const http = require("http");
+const express = require("express");
+const app = express();
 const PORT = 8080
 
-function handleRequest(request, response) {
-    response.end("It works!! Path Hit: " + request.url);
-}
+// middleware
 
-const server = http.createServer(handleRequest);
 
-server.listen(PORT, function () {
-    console.log("Server listening on: http://localhost:" + PORT);
-})
+// routes
+app.get("/", function(req, res){
+    res.send("welcome!");
+});
+app.post("/", function(req, res){
+    res.send("welcome!");
+});
+app.put("/", function(req, res){
+    res.send("welcome!");
+});
+app.delete("/", function(req, res){
+    res.send("welcome!");
+});
+
+
+// only use this route at the bottom of the route list; it catches everything after forward slash and returns 404
+app.get("*", function(req, res){
+    res.send("404 not found!");
+});
+
+app.listen(PORT, ()=>  console.log("App is listening on " + PORT));
