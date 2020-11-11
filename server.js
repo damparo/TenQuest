3e1d115c433153e49ea9b42640997f51cea00589
Dennis Amparo <dennisamparo11@gmail.com>
2020-11-11 10:28:46 -0700

    building server side code

---
+++ server.js
@@ -0,0 +1,12 @@
+const http = require("http");
+const PORT = 8080
+
+function handleRequest(request, response) {
+    response.end("It works!! Path Hit: " + request.url);
+}
+
+const server = http.createServer(handleRequest);
+
+server.listen(PORT, function () {
+    console.log("Server listening on: http://localhost:" + PORT);
+})
